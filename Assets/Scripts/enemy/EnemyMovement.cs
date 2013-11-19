dc3f068151c36f9245c9581ef2a5e1b58c0424df
Snakybo (Laptop) <krol.kevin@gmail.com>
2013-11-19 17:09:58 +0100

    Added player and enemy with basic AI

---
+++ Assets/Scripts/enemy/EnemyMovement.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMovement:MonoBehaviour {
+	public float walkSpeed;
+
+	public int hitPoints;
+	public int damage;
+
+	private bool hasAggro;
+	private bool seesPlayer;
+
+	private GameObject player;
+
+	private Vector3 lastKnownPosition;
+
+	private EnemySearch enemySearch;
+
+	void Start () {
+		player = GameObject.Find("Player");
+
+		enemySearch = GetComponent<EnemySearch>();
+	}
+
+	void Update() {
+		RaycastHit2D hit = Physics2D.Raycast(new Vector2(transform.position.x + 0.5f, transform.position.y - 1.1f), -Vector2.up);
+
+		if (hit) {
+			if(hit.collider.gameObject.name == "Player") {
+				hasAggro = true;
+				lastKnownPosition = player.transform.position;
+			}
+		} else {
+			seesPlayer = false;
+
+			if (Vector3.Distance(transform.position, lastKnownPosition) < 1) {
+				enemySearch.Search = true;
+
+				if(enemySearch.Found) {
+					seesPlayer = true;
+					hasAggro = true;
+				} else {
+					hasAggro = false;
+				}
+			}
+		}
+	}
+
+	void FixedUpdate () {
+		bool move = false;
+
+		if(hasAggro && seesPlayer) {
+			transform.LookAt(player.transform.position);
+			move = true;
+		} else if(hasAggro && !seesPlayer) {
+			transform.LookAt(lastKnownPosition);
+			move = true;
+		}
+
+		if(move) {
+			Vector2 speed = Vector2.zero;
+
+			transform.Rotate(0, 90, 0);
+			
+			speed = -Vector2.up * walkSpeed;
+			
+			rigidbody2D.velocity = speed * Time.deltaTime;
+		} else {
+			rigidbody2D.velocity = Vector2.zero;
+		}
+	}
+}
---
+++ Assets/Scripts/player/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement:MonoBehaviour {
+	public float walkSpeed;
+
+	void Start () {
+	
+	}
+
+	void FixedUpdate () {
+		Vector2 speed = Vector2.zero;
+
+		if(Input.GetKey("w")) {
+			speed.y = walkSpeed;
+		} else if(Input.GetKey("s")) {
+			speed.y = -walkSpeed;
+		}
+
+		if(Input.GetKey("d")) {
+			speed.x = walkSpeed;
+		} else if(Input.GetKey("a")) {
+			speed.x = -walkSpeed;
+		}
+
+		rigidbody2D.velocity = speed * Time.deltaTime;
+	}
+}
