5484cec1f91107a149cb755f6fc42088d3395bd9
Nolimet <nolimet.stamdata.nl>
2013-11-25 17:23:23 +0100

    Added shake

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -7,12 +7,37 @@ public class CameraController:MonoBehaviour {
 	public float distance = 0.0f;
 	private float height = 3.0f;
 	private float damping = 5.0f;
-	
+
+	public bool shake =false;
+	private float shakeTime = 0.0f;
+	public float maxShakeTime = 0.3f;
+	private Vector3 beforeShakeposs;
+	public float shakePower = 0.2f;
+	void Start(){
+		shakeTime=maxShakeTime;
+	}
+
 	void FixedUpdate() {
+		transform.position=beforeShakeposs;
+
 		if(target != null){
 			Vector3 wantedPosition;
 			wantedPosition = target.TransformPoint(0, height - 2, distance);
 			transform.position = Vector3.Lerp (transform.position, wantedPosition, Time.deltaTime * damping);
+		}
+
+		beforeShakeposs = transform.position;
+		if(shake){
+
+			if(shakeTime>0){
+				transform.position += new Vector3(1,0,0)*Random.Range(-shakePower,shakePower);
+				shakeTime-=Time.deltaTime;
+			}
+			else
+			{
+				shake=false;
+				shakeTime=maxShakeTime;
+			}
 		}
 	}
 }
