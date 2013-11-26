85e00c1a67da2a2d2b83d8fc0cd72bd89847d3e3
Snakybo (Laptop) <krol.kevin@gmail.com>
2013-11-26 11:22:42 +0100

    Added cheats and refactor

---
+++ Assets/Scripts/Cheats.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cheats:MonoBehaviour {
+	private string[] lights = new string[] {"l", "i", "g", "h", "t", "s"};
+
+	private int index = 0;
+
+	void Update() {
+		if(Input.anyKeyDown) {
+			if(Input.GetKeyDown(lights[index])) {
+				index++;
+			} else {
+				index = 0;
+			}
+		}	
+
+		if(index == lights.Length) {
+			Light light = GameObject.Find("Main Light").GetComponent<Light>();
+
+			light.enabled = !light.enabled;
+			index = 0;
+		}
+	}
+}
