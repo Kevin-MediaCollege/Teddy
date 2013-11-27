f34f0c806a41797c4adfceab428f84a2c6427935
Nolimet <nolimet.stamdata.nl>
2013-11-27 10:18:07 +0100

    added mainMenu

---
+++ Assets/Scripts/OnMouseClick.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OnMouseClick : MonoBehaviour {
+
+	private bool wasClicked;
+	public int LoadLevel;
+	
+	void OnMouseDown() {
+		wasClicked = true;
+	}
+	
+	void OnMouseUp() {
+		wasClicked = false;
+		Application.LoadLevel(LoadLevel);
+	}
+	/*
+	void OnMouseEnter() {
+		if (wasClicked) {
+			Activate();
+		}
+	}
+	
+	void OnMouseExit() {
+		Deactivate();
+	}
+	
+	void Activate() {
+		renderer.material.color = Color.red;
+	}
+	
+	void Deactivate() {
+		renderer.material.color = Color.white;
+	}*/
+}
