958563e4dbda361bf5839457ed23ecc35d5d7a01
Snakybo (Laptop) <krol.kevin@gmail.com>
2013-11-29 10:52:47 +0100

    Fixed menu's

---
+++ Assets/Scripts/ButtonHandler.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonHandler:MonoBehaviour {
+	public bool ExitGame = false;
+
+	public int LoadLevel;
+
+	public bool hasRenderer;
+
+	private Color newColor;
+	private Color originalColor;
+
+	void Start(){
+		if(hasRenderer){
+			newColor = new Color(0.9f, 0.4f, 0.4f);
+			originalColor = gameObject.renderer.material.color;
+		}
+	}
+
+	void OnMouseOver() {
+		if(hasRenderer){
+			renderer.material.color = newColor;
+		}
+	}
+
+	void OnMouseExit() {
+		if(hasRenderer){
+			renderer.material.color = originalColor;
+		}
+	}
+
+	void OnMouseDown() {
+		if(!ExitGame){
+			Application.LoadLevel(LoadLevel);
+		} else{
+			if(!Application.isEditor && !Application.isWebPlayer) {
+				Application.CancelQuit();
+			}
+		}
+	}
+}
