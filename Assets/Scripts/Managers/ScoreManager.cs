2a1363e24fb34a9e1047c17cf27e25d1c98f411e
Snakybo (Laptop) <krol.kevin@gmail.com>
2013-12-02 16:30:10 +0100

    Added second level

---
+++ Assets/Scripts/Managers/ScoreManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoreManager:MonoBehaviour {
+	public float score;
+	public float kills;
+
+	void Start () {
+		DontDestroyOnLoad(gameObject);
+
+		score = 0;
+		kills = 0;
+	}
+}
