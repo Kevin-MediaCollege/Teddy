ee3d837b60cd70a5c83085da8be3631c7081124e
Jesse Stam <nolimet.stamdata.nl>
2013-12-02 21:06:06 +0100

    added a bit of button animation

--- Assets/Scripts/Managers/ButtonManager.cs
+++ Assets/Scripts/Managers/ButtonManager.cs
@@ -10,23 +10,27 @@ public class ButtonManager:MonoBehaviour {
 
 	private Color newColor;
 	private Color originalColor;
+	private Vector3 orignalPos;
 
 	void Start(){
 		if(hasRenderer){
-			newColor = new Color(0.9f, 0.4f, 0.4f);
+			newColor = new Color(0.9f, 0.4f, 0.9f);
 			originalColor = gameObject.renderer.material.color;
+			orignalPos=transform.position;
 		}
 	}
 
 	void OnMouseOver() {
 		if(hasRenderer){
 			renderer.material.color = newColor;
+			transform.position=orignalPos+new Vector3(0,0.05f);
 		}
 	}
 
 	void OnMouseExit() {
 		if(hasRenderer){
 			renderer.material.color = originalColor;
+			transform.position=orignalPos;
 		}
 	}
 
