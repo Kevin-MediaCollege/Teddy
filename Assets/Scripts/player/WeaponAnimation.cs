f0c8fadc13a70c2b3013f40d336a3c727c346f3d
Snakybo (Laptop) <krol.kevin@gmail.com>
2013-12-02 15:10:52 +0100

    Fixed enemies

--- Assets/Scripts/player/WeaponAnimation.cs
+++ Assets/Scripts/player/WeaponAnimation.cs
@@ -1,38 +1,36 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class WeaponAnimation : MonoBehaviour {
-
-	private Quaternion startRot;
+public class WeaponAnimation:MonoBehaviour {
 	private float animationLenght = 0.8f;
+
 	private float animationTimeLeft;
+	private Quaternion startRot;
 	private bool animationDone;
-	// Use this for initialization
-	void Start () {
-		startRot=transform.rotation;
-		//transform.rotation=new Quaternion(0,0,20,0);
-	}
 	
-	// Update is called once per frame
+	void Start() {
+		startRot = transform.rotation;
+	}
+
 	void Update () {
-		if(Input.GetMouseButtonDown(0)){
+		if(Input.GetMouseButtonDown(0)) {
 			animationTimeLeft = animationLenght; 
-			transform.localRotation=startRot;
-			animationDone=false;
+			transform.localRotation = startRot;
+			animationDone = false;
 		}
 
-		if(animationTimeLeft>0&&animationTimeLeft<((animationLenght/2)*1.9f)){
+		if(animationTimeLeft > 0 && animationTimeLeft < ((animationLenght / 2) * 1.9f)) {
 			animationTimeLeft -= Time.deltaTime;
-			transform.Rotate(0f,0f,7.5f);
+			transform.Rotate(0f, 0f, 7.5f);
 		}
-		if(animationTimeLeft>0&&animationTimeLeft>((animationLenght/2)*1.9f)){
+		if(animationTimeLeft > 0 && animationTimeLeft > ((animationLenght / 2) * 1.9f)) {
 			animationTimeLeft -= Time.deltaTime;
-			transform.Rotate(0f,0f,-80f);
+			transform.Rotate(0f, 0f, -80f);
 		}
-		if(animationTimeLeft<=0&&!animationDone)
+		if(animationTimeLeft <= 0 && !animationDone)
 		{
-			transform.localRotation=startRot;
-			animationDone=true;
+			transform.localRotation = startRot;
+			animationDone = true;
 		}
 	}
 }
